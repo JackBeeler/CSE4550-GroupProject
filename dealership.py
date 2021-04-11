d17faee2dcd0454ca706adfb2a925382b628f49e
JackBeeler <57203954+JackBeeler@users.noreply.github.com>
2021-04-11 09:14:48 -0700

    Update dealership.py

--- dealership.py
+++ dealership.py
@@ -39,7 +39,7 @@ def signup():
         elif not email or not password:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute('INSERT INTO customer_login VALUES (NULL, %s, %s)', (email, password))
+            cursor.execute('INSERT INTO customer_login VALUES (NULL, %s, %s, %s)', (email, password))
             mysql.connection.commit()
             msg = "You have successfully registered!"
     elif request.method == 'POST':
