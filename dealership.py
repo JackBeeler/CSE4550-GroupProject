301976786d5e7520dfb801275eadd1a03a2391fb
JackBeeler <57203954+JackBeeler@users.noreply.github.com>
2021-04-26 10:22:27 -0700

    Update dealership.py

--- dealership.py
+++ dealership.py
@@ -124,6 +124,25 @@ def about():
         return render_template('about.html')
         
 
+        
+        
+        @app.route('/searchresults/searchresults/hatchbacks')
+def searchresultshatchbacks():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM inventory  WHERE body_style = "hatchback"')
+    data = cursor.fetchall()
+    numRows = cursor.rowcount
+
+    if 'username' in session:  
+        username5 = session['username'] 
+        return render_template('SearchResults.html',homepageusername = username5, data=data, numRows=numRows)
+    else:
+        return render_template('SearchResults.html', data=data, numRows=numRows) 
+        
+        
+        
+        
+        
 @app.route('/searchresults/hatchbacks')
 def searchresultshatchbacks():
     cursor = mysql.connection.cursor()
