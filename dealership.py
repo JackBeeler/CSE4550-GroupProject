1475787d867de4acf2913175f535543ca4c8f3aa
JackBeeler <57203954+JackBeeler@users.noreply.github.com>
2021-04-27 20:32:45 -0700

    Update dealership.py

--- dealership.py
+++ dealership.py
@@ -109,7 +109,7 @@ def login():
              redirect(url_for('homepagelogged', msg=msg, data = data))
             else: 
              msg = "Wrong username/password"
-            return render_template('LogIn.html', msg=msg)
+    return render_template('LogIn.html', msg=msg)
 
 
 
