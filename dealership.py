5c3bdce107c7b9cc04b123e7b3125c4bf5c3b647
JackBeeler <57203954+JackBeeler@users.noreply.github.com>
2021-04-27 20:10:49 -0700

    Update dealership.py

--- dealership.py
+++ dealership.py
@@ -106,7 +106,7 @@ def login():
             session['loggedin'] = True
             session['employee_id'] = employeelogin['employee_id']
             session['username'] = employeelogin['username']
-            return redirect(url_for('homepagelogged'))
+            return render_template('homepagelogged', msg=msg)
         else: 
             msg = "Wrong username/password"
     return render_template('LogIn.html', msg=msg)
