151f016fdafaeace00fa071bc78dfc934ed963c4
JackBeeler <57203954+JackBeeler@users.noreply.github.com>
2021-04-29 10:51:30 -0700

    Update dealership.py

--- dealership.py
+++ dealership.py
@@ -953,9 +953,8 @@ def favorites():
                if data:
                   numRows = cursor.rowcount
                   return render_template('favorites.html',homepageusername = username6, msg = msg, data=data, numRows=numRows)
-        elif not data:
-            msg = "You do not have any favorites"
-            return render_template('favorites.html',homepageusername = username6, msg = msg, data=data, numRows=0)
+               else: 
+                  return render_template('favorites.html',homepageusername = username6, msg = msg, data=data, numRows=0)
      
         
      else:
