cf1e8e38e09c0e7558d27bcd6ab37cb31aa61760
JackBeeler <57203954+JackBeeler@users.noreply.github.com>
2021-05-01 16:32:34 -0700

    Update dealership.py

--- dealership.py
+++ dealership.py
@@ -1377,7 +1377,7 @@ def addToFavorites():
         
         cursor2 = mysql.connection.cursor()
         cursor2.execute('INSERT INTO jackfavorites VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', (vin1, make1, model1, year1, color1, mileage1, price1, our_price1, transmission1, body_style1,car_photo1, UserID,))
-         mysql.connection.commit()
+        mysql.connection.commit()
           
           
         return redirect(url_for('favorites'))
