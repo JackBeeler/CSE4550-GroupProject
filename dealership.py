43a1604d9b287b8752a6ae90244684241402d225
JackBeeler <57203954+JackBeeler@users.noreply.github.com>
2021-05-10 19:19:14 -0700

    Update dealership.py

--- dealership.py
+++ dealership.py
@@ -85,15 +85,7 @@ def signup():
 def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        
-     
-        regexStr = r'^([^@]+)@[^@]+$'
-        emailStr = request.form['username']
-        matchobj = re.search(regexStr, emailStr)
-        if not matchobj is None:
-            username =  matchobj.group(1)
-        else:
-            username = request.form['username']    
+        username = request.form['username']
         password = request.form['password']
         cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cur.execute('SELECT * FROM customer_login WHERE username = %s AND password = %s', (username, password,))
